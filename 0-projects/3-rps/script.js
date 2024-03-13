002a7a8fe3fd4d6e2b041652954c385fe53446ec
futurebound <mitchellm1618@gmail.com>
2024-03-12 21:07:23 -0700

    feat: added basic RPS ui for playing full game

--- 0-projects/3-rps/script.js
+++ 0-projects/3-rps/script.js
@@ -21,40 +21,50 @@ function playRound(playerSelection, computerSelection) {
 
   // compare selection to see if win (1) / lose (-1) / tie (0)
     // paper > rock, rock > scissor, scissor > paper
-  let outcome = 0
+  let result = 0
   let message = "It's a tie!"
   if (playerSelection !== computerSelection) {
     if (playerSelection === "rock") {
       if (computerSelection === "scissors") {
         message = "You Win! Rock beats Scissors"
-        outcome = 1
+        result = 1
       } else {
         message = "You Lose! Paper beats Rock"
-        outcome = -1
+        result = -1
       }
 
     } else if (playerSelection === "paper") {
       if (computerSelection === "rock") {
         message = "You Win! Paper beats Rock"
-        outcome = 1
+        result = 1
       } else {
         message = "You Lose! Scissors beats Paper"
-        outcome = -1
+        result = -1
       }
 
     } else { // playerSelection = "scissors"
       if (computerSelection === "paper") {
         message = "You Win! Scissors beats Paper"
-        outcome = 1
+        result = 1
       } else {
         message = "You Lose! Rock beats Scissors"
-        outcome = -1
+        result = -1
       }
     }
   }
 
-  console.log(message)
-  return outcome
+  const resultsDiv = document.querySelector("#results")
+  resultsDiv.textContent = message
+
+  const playerScore = document.querySelector("#player-score")
+  const oldPlayerScore = parseInt(playerScore.textContent)
+  playerScore.textContent = result + parseInt(oldPlayerScore)
+
+  const computerScore = document.querySelector("#computer-score")
+  const oldComputerScore = parseInt(computerScore.textContent)
+  computerScore.textContent = (result * -1) + oldComputerScore
+
+  return result
 }
 
 // plays an entire 5 round game
@@ -67,7 +77,7 @@ function playGame() {
     console.log(`round ${i}: difference = ${difference}`)
   }
 
-  // report outcome
+  // report result
   if (difference < 0) {
     console.log("You lost the game!")
   } else if (difference === 0) {
@@ -77,24 +87,32 @@ function playGame() {
   }
 }
 
+const rockButton = document.querySelector(".rock")
+rockButton.addEventListener("click", () => {
+  playRound("Rock", getComputerChoice())
+  checkGameOver()
+})
 
-/*
-function testPlayRound() {
-  const p1 = "ROCK"
-  console.log(playRound(p1, "Rock"))
-  console.log(playRound(p1, "Paper"))
-  console.log(playRound(p1, "Scissors"))
+const paperButton = document.querySelector(".paper")
+paperButton.addEventListener("click", () => {
+  playRound("Paper", getComputerChoice())
+  checkGameOver()
+})
+
+const scissorsButton = document.querySelector(".scissors")
+scissorsButton.addEventListener("click", () => {
+  playRound("Scissors", getComputerChoice())
+  checkGameOver()
+})
+
+function checkGameOver() {
+  const playerScore = document.querySelector("#player-score")
+  const computerScore = document.querySelector("#computer-score")
+
+  if (parseInt(playerScore.textContent) >= 5) {
+    alert("Congrats! You won the game!")
+  } else if (parseInt(computerScore.textContent) >= 5) {
+    alert("Ooh! Too bad you lose!")
+  }
   
-  const p2 = "pApER"
-  console.log(playRound(p2, "Rock"))
-  console.log(playRound(p2, "Paper"))
-  console.log(playRound(p2, "Scissors"))
-  
-  const p3 = "sciSSORs"
-  console.log(playRound(p3, "Rock"))
-  console.log(playRound(p3, "Paper"))
-  console.log(playRound(p3, "Scissors"))
 }
-*/
-
-playGame()
