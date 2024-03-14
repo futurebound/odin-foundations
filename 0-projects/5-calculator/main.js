3151e54b0f2a808c13ec919ed699232578687b8b
futurebound <mitchellm1618@gmail.com>
2024-03-14 10:50:29 -0700

    feat: basic operations with input validation

--- 0-projects/5-calculator/main.js
+++ 0-projects/5-calculator/main.js
@@ -20,7 +20,7 @@ function modulo(a, b) {
   return a % b
 }
 
-function operate(operator, a, b) {
+function operate(a, b, operator) {
   if (operator === "+") {
     return add(a, b)
   } else if (operator === "-") {
@@ -37,18 +37,55 @@ function operate(operator, a, b) {
 }
 
 const display = document.querySelector(".display-text")
-console.log(display)
+// console.log(display)
 
-let numbers = document.querySelectorAll(".number")
-numbers = Array.from(numbers)
-// console.log(numbers)
-numbers.map(number => {
+const numbers = document.querySelectorAll(".number")
+numbers.forEach(number => {
   number.addEventListener("click", () => {
     display.textContent += number.textContent
+  })
+})
+
+const operators = document.querySelectorAll(".operator")
+operators.forEach(operator => {
+  operator.addEventListener("click", () => {
+    display.textContent += ` ${operator.textContent} `
   })
 })
 
 const clear = document.querySelector(".clear")
 clear.addEventListener("click", () => {
   display.textContent = ""
+})
+
+const submit = document.querySelector(".submit")
+submit.addEventListener("click", () => {
+  let input = display.textContent
+  input = input.split(" ")
+  // console.log(`before: ${input}`)
+  // console.log(`isNaN: ${isNaN(parseInt(input[2]))}`)
+  if (input.length < 3 || isNaN(parseInt(input[2]))) {
+    alert("please enter a valid equation to evaluate")
+
+  } else {
+    // spaces only occur with operators, so loop until input array only a single
+    //  element / number remaining
+    // e.g. ["12", "+", "3"] => ["15"]
+    while (input.length > 1) {
+      const a = parseInt(input[0])
+      const b = parseInt(input[2])
+      const operator = input[1]
+      const result = operate(a, b, operator)
+  
+      input[2] = result
+      input = input.slice(2)
+  
+      // console.log(`during: ${input}`)
+    }
+  
+    // console.log(`after: ${input}`)
+  
+    display.textContent = input
+
+  }
 })
