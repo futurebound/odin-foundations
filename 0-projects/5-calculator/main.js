11814cb87e802586c78dcf8763d11d2a1209f60b
futurebound <mitchellm1618@gmail.com>
2024-03-14 10:18:02 -0700

    feat: number and clear buttons modify display text

--- 0-projects/5-calculator/main.js
+++ 0-projects/5-calculator/main.js
@@ -16,6 +16,39 @@ function divide(a, b) {
   return a / b
 }
 
-function operate(operator, a, b) {
-  
+function modulo(a, b) {
+  return a % b
 }
+
+function operate(operator, a, b) {
+  if (operator === "+") {
+    return add(a, b)
+  } else if (operator === "-") {
+    return subtract(a, b)
+  } else if (operator === "*") {
+    return multiply(a, b)
+  } else if (operator === "/") {
+    return divide(a, b)
+  } else if (operator === "%") {
+    return modulo(a, b)
+  }
+
+  return "ERROR - unknown operation"
+}
+
+const display = document.querySelector(".display-text")
+console.log(display)
+
+let numbers = document.querySelectorAll(".number")
+numbers = Array.from(numbers)
+// console.log(numbers)
+numbers.map(number => {
+  number.addEventListener("click", () => {
+    display.textContent += number.textContent
+  })
+})
+
+const clear = document.querySelector(".clear")
+clear.addEventListener("click", () => {
+  display.textContent = ""
+})
